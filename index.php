f232216bdf40fb3f724e460ccf15091c2f1b6a06
Vologin <mikimoto999@gmail.com>
2018-04-06 13:44:34 +0700

    Добавил папку шрифтов

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Справочник монтажника</title>
-<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/style.css?ver=1.3">
 </head>
 <body>
 	<div class="main">
@@ -12,13 +12,13 @@
 			<div class="info-menu">
 				<!--<div class="info-menu__strelka-mts-shpd"></div>-->
 				<div class="info-menu__link-schpd info-menu__link-schpd_shadow">
-					<a href="#" class="info-menu__text-shpd">ШПД</a>
+					<a href="#" class="info-menu__text-shpd default-font">ШПД</a>
 				</div>
 				<div class="info-menu__empty-block"></div>
 				
 				<!--<div class="info-menu__strelka-mts-ktv"></div>-->
 				<div class="info-menu__link-ktv info-menu__link-schpd_shadow">
-					<a href="#" class="info-menu__text-ktv">КТВ</a>
+					<a href="#" class="info-menu__text-ktv default-font">КТВ</a>
 				</div>
 			</div>
 		</div>
@@ -27,12 +27,12 @@
 			<div class="logo-cherdak"></div>
 			<div class="cherdak-menu">
 				<div class="cherdak-menu__link-center info-menu__link-schpd_shadow">
-					<a href="#" class="cherdak-menu__text-center">Центр</a>
+					<a href="#" class="cherdak-menu__text-center default-font">Центр</a>
 				</div>
 				<!--<div class="cherdak-menu__strelka-cherdak-center"></div>-->
 				<div class="info-menu__empty-block"></div>
 				<div class="cherdak-menu__link-zapsib info-menu__link-schpd_shadow">
-					<a href="#" class="cherdak-menu__text-zapsib ">Запсиб</a>
+					<a href="#" class="cherdak-menu__text-zapsib default-font">Запсиб</a>
 				</div>
 				<!--<div class="cherdak-menu__strelka-cherdak-zapsib"></div>-->
 			</div>
